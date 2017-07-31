7f3f59abe87c6d0a861c3323b326ca38d56c99b4
Brian McCollum <bmccollumw@gmail.com>
2017-07-30 19:58:17 -0500

    test 1

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // a0:02:dc:9b:74:3d - MAC address
+var exec = require("child_process").exec;
 
 var dash = require("dash-button");
 var macAddress = "a0:02:dc:9b:74:3d";
 var button = new dash(macAddress);
 var subscription = button.addListener(function(){
     console.log("puta");
+    exec("gpio -g mode 14 out && gpio -g toggle 14");
 });
 
 
