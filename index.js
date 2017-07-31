467be0a53f75c11b307ffee6cd9275f063d6de71
Brian McCollum <bmccollumw@gmail.com>
2017-07-30 19:00:09 -0500

    before pi

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Meow cunt");
+// a0:02:dc:9b:74:3d - MAC address
+
+var dash = require("dash-button");
+var macAddress = "a0:02:dc:9b:74:3d";
+var button = new dash(macAddress);
+var subscription = button.addListener(function(){
+    console.log("puta");
+});
+
+
+console.log("silly cunt");
+//console.log(dash);
